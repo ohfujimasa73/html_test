4149c1dbac7d6451ce5df8b03e35afc0c9bd8355
ohfujimasa73 <ohfuji.masa73@gmail.com>
2022-07-30 07:35:18 +0900

    初回コミット

---
+++ js/src.js
@@ -0,0 +1,12 @@
+
+function clickBtn2(){
+      const p2 = document.getElementById("txt1");
+    
+      if(p2.style.display=="none"){
+        // hiddenで非表示
+        p2.style.display ="block";
+      }else{
+        // visibleで表示
+        p2.style.display ="none";
+      }
+}
